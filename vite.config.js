6cbcd72bcc5a237b23298923388c3707c00073f9
habibur rahman <habibur.rahman@elementzerolabs.com>
2024-04-15 00:29:45 +0600

    df

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
-    assetsInclude: ['**/*.glb'],
-    base: '/',
+    assetsInclude: ["**/*.glb"],
+    base: "/",
+  };
+
+  if (command !== "serve") {
+    config.base = "/habib/";
   }
 
-  if (command !== 'serve') {
-    config.base = '/hoanghuyduc/'
-  }
-
-  return config
-})
+  return config;
+});
